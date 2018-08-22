e17883b6440aa02659dffa80a0ddfcdfb2cf0540
kdoerges <kdoerges@cycronic.de>
2018-08-22 11:39:44 +0200

    start ping command

---
+++ src/main/java/org/vebqa/vebtal/tn3270/Ping.java
@@ -0,0 +1,24 @@
+package org.vebqa.vebtal.tn3270;
+
+import org.vebqa.vebtal.command.AbstractCommand;
+import org.vebqa.vebtal.model.CommandType;
+import org.vebqa.vebtal.model.Response;
+import org.vebqa.vebtal.tn3270restserver.TN3270TestAdaptionPlugin;
+
+public class Ping extends AbstractCommand {
+
+	public Ping(String aCommand, String aTarget, String aValue) {
+		super(aCommand, aTarget, aValue);
+		this.type = CommandType.ACTION;
+	}
+
+	@Override
+	public Response executeImpl(Object aDriver) {
+
+		Response tResponse = new Response();
+		tResponse.setCode("0");
+		tResponse.setMessage("Service " + TN3270TestAdaptionPlugin.ID + " is up and running.");
+		
+		return tResponse;
+	}
+}
